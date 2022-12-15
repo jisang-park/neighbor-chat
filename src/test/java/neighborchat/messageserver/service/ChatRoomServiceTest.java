4371a9f26397333b9e57e70be12896ddd922abe5
jisang-park <chlrh9009@gmail.com>
2022-12-16 04:50:06 +0900

    [#5] 거리 검색 API Service 테스트 코드 추가

--- src/test/java/neighborchat/messageserver/service/ChatRoomServiceTest.java
+++ src/test/java/neighborchat/messageserver/service/ChatRoomServiceTest.java
@@ -12,8 +12,10 @@ import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ChatRoomServiceTest {
@@ -26,32 +28,52 @@ class ChatRoomServiceTest {
 
     @Test
     void createChatRoomTest() {
-        ChatRoom chatRoom = ChatRoom.builder()
-                .leaderId("leaderId")
-                .subject("subject")
-                .name("name")
-                .location(new GeoJsonPoint(123.12345, 37.653213))
-                .createdTime(LocalDateTime.now())
-                .lastChatTime(LocalDateTime.now())
-                .banned(new ArrayList<>())
-                .build();
-
-        ChatRoom savedChatRoom = ChatRoom.builder()
-                .id("chatRoomId")
-                .leaderId("leaderId")
-                .subject("subject")
-                .name("name")
-                .location(new GeoJsonPoint(123.12345, 37.653213))
-                .createdTime(LocalDateTime.now())
-                .lastChatTime(LocalDateTime.now())
-                .banned(new ArrayList<>())
-                .build();
+        ChatRoom chatRoom = ChatRoom.builder().leaderId("leaderId").subject("subject").name("name").location(new GeoJsonPoint(123.456789, 37.123456)).createdTime(LocalDateTime.now()).lastChatTime(LocalDateTime.now()).banned(new ArrayList<>()).build();
+        ChatRoom savedChatRoom = ChatRoom.builder().id("chatRoomId").leaderId("leaderId").subject("subject").name("name").location(new GeoJsonPoint(123.456789, 37.123456)).createdTime(LocalDateTime.now()).lastChatTime(LocalDateTime.now()).banned(new ArrayList<>()).build();
 
         when(chatRoomRepository.save(chatRoom)).thenReturn(savedChatRoom);
 
         String chatRoomId = chatRoomService.createChatRoom(chatRoom);
 
         Assertions.assertThat(chatRoomId).isEqualTo("chatRoomId");
+    }
+
+    @Test
+    void searchChatRoomsWithoutSubjectTest() {
+        ArrayList<ChatRoom> chatRooms = new ArrayList<>();
+
+        ChatRoom chatRoom1 = ChatRoom.builder().leaderId("leaderId").subject("subject").name("name").location(new GeoJsonPoint(123.456789, 37.123456)).createdTime(LocalDateTime.now()).lastChatTime(LocalDateTime.now()).banned(new ArrayList<>()).build();
+        ChatRoom chatRoom2 = ChatRoom.builder().leaderId("leaderId").subject("subject").name("name").location(new GeoJsonPoint(123.456789, 37.123456)).createdTime(LocalDateTime.now()).lastChatTime(LocalDateTime.now()).banned(new ArrayList<>()).build();
+
+        chatRooms.add(chatRoom1);
+        chatRooms.add(chatRoom2);
+
+        when(chatRoomRepository.findByLocationNear(any(), any())).thenReturn(chatRooms);
+
+        List<ChatRoom> results = chatRoomService.searchChatRooms(123.456789, 37.123456, 0.5);
+
+        Assertions.assertThat(results).containsExactly(chatRoom1, chatRoom2);
+        verify(chatRoomRepository, only()).findByLocationNear(any(), any());
+        verify(chatRoomRepository, never()).findByLocationNearAndSubject(any(), any(), anyString());
+    }
+
+    @Test
+    void searchChatRoomsWithSubjectTest() {
+        ArrayList<ChatRoom> chatRooms = new ArrayList<>();
+
+        ChatRoom chatRoom1 = ChatRoom.builder().leaderId("leaderId").subject("subject").name("name").location(new GeoJsonPoint(123.456789, 37.123456)).createdTime(LocalDateTime.now()).lastChatTime(LocalDateTime.now()).banned(new ArrayList<>()).build();
+        ChatRoom chatRoom2 = ChatRoom.builder().leaderId("leaderId").subject("subject").name("name").location(new GeoJsonPoint(123.456789, 37.123456)).createdTime(LocalDateTime.now()).lastChatTime(LocalDateTime.now()).banned(new ArrayList<>()).build();
+
+        chatRooms.add(chatRoom1);
+        chatRooms.add(chatRoom2);
+
+        when(chatRoomRepository.findByLocationNearAndSubject(any(), any(), eq("subject"))).thenReturn(chatRooms);
+
+        List<ChatRoom> results = chatRoomService.searchChatRooms("subject", 123.456789, 37.123456, 0.5);
+
+        Assertions.assertThat(results).containsExactly(chatRoom1, chatRoom2);
+        verify(chatRoomRepository, never()).findByLocationNear(any(), any());
+        verify(chatRoomRepository, only()).findByLocationNearAndSubject(any(), any(), eq("subject"));
     }
 
 }
