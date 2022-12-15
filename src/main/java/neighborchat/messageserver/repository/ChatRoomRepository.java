7d83e81ac7ccfee1c79978e6a4daebac1972b275
jisang-park <chlrh9009@gmail.com>
2022-12-16 04:47:41 +0900

    [#5] 대화방 거리 검색기능 추가

--- src/main/java/neighborchat/messageserver/repository/ChatRoomRepository.java
+++ src/main/java/neighborchat/messageserver/repository/ChatRoomRepository.java
@@ -1,7 +1,15 @@
 package neighborchat.messageserver.repository;
 
 import neighborchat.messageserver.domain.ChatRoom;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
+
+    List<ChatRoom> findByLocationNear(Point location, Distance distance);
+    List<ChatRoom> findByLocationNearAndSubject(Point location, Distance distance, String subject);
+
 }
--- src/main/java/neighborchat/messageserver/service/ChatRoomService.java
+++ src/main/java/neighborchat/messageserver/service/ChatRoomService.java
@@ -3,7 +3,12 @@ package neighborchat.messageserver.service;
 import lombok.RequiredArgsConstructor;
 import neighborchat.messageserver.domain.ChatRoom;
 import neighborchat.messageserver.repository.ChatRoomRepository;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Metrics;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +18,18 @@ public class ChatRoomService {
 
     public String createChatRoom(ChatRoom chatRoom) {
         return chatRoomRepository.save(chatRoom).getId();
+    }
+
+    public List<ChatRoom> searchChatRooms(double longitude, double latitude, double distance) {
+        GeoJsonPoint center = new GeoJsonPoint(longitude, latitude);
+        Distance radius = new Distance(distance, Metrics.KILOMETERS);
+        return chatRoomRepository.findByLocationNear(center, radius);
+    }
+
+    public List<ChatRoom> searchChatRooms(String subject, double longitude, double latitude, double distance) {
+        GeoJsonPoint center = new GeoJsonPoint(longitude, latitude);
+        Distance radius = new Distance(distance, Metrics.KILOMETERS);
+        return chatRoomRepository.findByLocationNearAndSubject(center, radius, subject);
     }
 
 }
